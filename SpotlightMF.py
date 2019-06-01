210a65ad20100d033e8d08f1d073b0d3f7c8ed86
Korres-Timos <timoskorres@hotmail.com>
2019-06-01 21:22:22 +0100

    used Spotlight

---
+++ SpotlightMF.py
@@ -0,0 +1,14 @@
+
+from spotlight.cross_validation import random_train_test_split
+from spotlight.datasets.movielens import get_movielens_dataset
+from spotlight.evaluation import rmse_score
+from spotlight.factorization.implicit import ImplicitFactorizationModel
+
+​dataset = get_movielens_dataset(variant='20M')
+​
+train, test = random_train_test_split(dataset)
+​
+model = ImplicitFactorizationModel(n_iter=1)
+model.fit(train,verbose=True)
+​
+rmse = rmse_score(model, test)
