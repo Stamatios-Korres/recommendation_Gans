5ee275864a678255e0e061e9a1a686c9bd4286cd
Korres-Timos <timoskorres@hotmail.com>
2019-06-02 17:51:31 +0100

    spotlight fit model

--- SpotlightMF.py
+++ SpotlightMF.py
@@ -4,11 +4,11 @@ from spotlight.datasets.movielens import get_movielens_dataset
 from spotlight.evaluation import rmse_score
 from spotlight.factorization.implicit import ImplicitFactorizationModel
 
-​dataset = get_movielens_dataset(variant='20M')
-​
+dataset = get_movielens_dataset(variant='20M')
 train, test = random_train_test_split(dataset)
-​
-model = ImplicitFactorizationModel(n_iter=1)
+model = ImplicitFactorizationModel(n_iter=5)
 model.fit(train,verbose=True)
-​
+
 rmse = rmse_score(model, test)
+
+
