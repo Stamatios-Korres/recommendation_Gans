af789b7cf7d6d0d35599fcf77d557773c2ecd020
Korres-Timos <timoskorres@hotmail.com>
2019-06-02 18:37:41 +0100

    spotlight fit model

--- SpotlightMF.py
+++ SpotlightMF.py
@@ -1,14 +1,25 @@
 
 from spotlight.cross_validation import random_train_test_split
 from spotlight.datasets.movielens import get_movielens_dataset
-from spotlight.evaluation import rmse_score
+from spotlight.evaluation import rmse_score,precision_recall_score
 from spotlight.factorization.implicit import ImplicitFactorizationModel
 
-dataset = get_movielens_dataset(variant='20M')
+import numpy as np
+
+dataset = get_movielens_dataset(variant='100K')
 train, test = random_train_test_split(dataset)
-model = ImplicitFactorizationModel(n_iter=5)
+print("Data loaded, ready to fit")
+model = ImplicitFactorizationModel(n_iter=100,embedding_dim=50,l2=.00001,learning_rate=0.001)
+print("Model set, training begins")
+
 model.fit(train,verbose=True)
 
-rmse = rmse_score(model, test)
+print("Model is ready, testing performance")
+
+# rmse = rmse_score(model, test)
+
+precision,recall = precision_recall_score(model, test)
+
+print("precision,recall :",np.mean(precision),np.mean(recall))
 
 
