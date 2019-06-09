33fcc831643a718c8ebbdbae4752f50ba61ed033
timos <s1877727@sms.ed.ac.uk>
2019-06-09 23:43:32 +0100

    Fixed mlp layer bug

--- mf_spotlight.py
+++ mf_spotlight.py
@@ -48,7 +48,8 @@ batch_size = args.batch_size
 
 # Choose training model
 if args.model == 'mlp':
-    layers = [2**x for x in reversed(range(2,int(np.log2(embedding_dim*2))+1))] 
+    top = np.log2(embedding_dim*2)
+    layers = [2**x for x in reversed(range(2,int(top)+1))] 
     technique = mlp(layers=layers,num_users=users,num_items=movies,embedding_dim = embedding_dim)
 else:
     technique = BilinearNet(users, movies, embedding_dim, sparse=False)
