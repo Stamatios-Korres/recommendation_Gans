5a3548ca43240ba41cdfdbae885428ee1ff6a3e4
Korres-Timos <timoskorres@hotmail.com>
2019-06-11 14:48:40 +0100

    Cluster

--- cluster_scripts/1m_matrix_factorization_regul.sh
+++ cluster_scripts/1m_matrix_factorization_regul.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 #SBATCH -N 1	  # nodes requested
 #SBATCH -n 1	  # tasks requested
-#SBATCH --partition=Interactive
+#SBATCH --partition=LongJobs
 #SBATCH --gres=gpu:1
 #SBATCH --mem=12000  # memory in Mb
-#SBATCH --time=0-02:59:59
+#SBATCH --time=0-03:59:59
 
 export CUDA_HOME=/opt/cuda-9.0.176.1/
 
