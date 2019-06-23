fda8c581b5af42c0b46ddaac01ff28f86a8ef563
timos <s1877727@sms.ed.ac.uk>
2019-06-23 16:22:16 +0100

    Improving project's structure

---
+++ cluster_scripts/10m/mlp/test_10m_mf.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+#SBATCH -N 1	  # nodes requested
+#SBATCH -n 1	  # tasks requested
+#SBATCH --partition=Short
+#SBATCH --gres=gpu:1
+#SBATCH --mem=12000  # memory in Mb
+#SBATCH --time=0-03:59:59
+
+export CUDA_HOME=/opt/cuda-9.0.176.1/
+
+export CUDNN_HOME=/opt/cuDNN-7.0/
+
+export STUDENT_ID=$(whoami)
+
+export LD_LIBRARY_PATH=${CUDNN_HOME}/lib64:${CUDA_HOME}/lib64:$LD_LIBRARY_PATH
+
+export LIBRARY_PATH=${CUDNN_HOME}/lib64:$LIBRARY_PATH
+
+export CPATH=${CUDNN_HOME}/include:$CPATH
+
+export PATH=${CUDA_HOME}/bin:${PATH}
+
+export PYTHON_PATH=$PATH
+
+mkdir -p /disk/scratch/${STUDENT_ID}
+
+export TMPDIR=/disk/scratch/${STUDENT_ID}/
+
+export TMP=/disk/scratch/${STUDENT_ID}/
+
+mkdir -p ${TMP}/datasets/
+
+export DATASET_DIR=${TMP}/datasets/
+
+rsync -ua --progress /home/${STUDENT_ID}/recommendations/datasets/movielens/ /disk/scratch/${STUDENT_ID}/datasets/movielens
+
+source /home/${STUDENT_ID}/miniconda3/bin/activate mlp
+
+echo activated mlp.
+
+cd /home/${STUDENT_ID}/recommendations/
+
+echo "changed to recommendation folder. Calling python"
+
+
+python3 test_model.py  --use_gpu "True" --map_recall 'True' \
+                         --batch_size 256 --dataset '10M' \
+                         --model mf --k 5 --neg_examples 5 \
+                         --experiment_name "neuM_16x25_model_10M" --on_cluster 'True'
+
+
