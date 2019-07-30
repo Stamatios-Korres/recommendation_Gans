a45cbe43eb48060497c97b57c5281a38ff73ae12
Timos Korres <timoskorres@hotmail.com>
2019-07-30 19:00:36 +0100

    Added dropout

--- spotlight/dnn_models/mlp.py
+++ spotlight/dnn_models/mlp.py
@@ -19,9 +19,10 @@ class MLP(nn.Module):
         
         for idx in range(len(layers)-1):
             self.layers.append(nn.Linear(layers[idx],layers[idx+1]))
-            self.layers.append(nn.BatchNorm1d(num_features=layers[idx+1]))
+            self.layers.append(nn.LeakyReLU(0.1,inplace=True))
             self.layers.append(nn.Dropout(0.5))
-            self.layers.append(nn.ReLU())
+            self.layers.append(nn.BatchNorm1d(num_features=layers[idx+1]))
+            
         self.layers.append(torch.nn.Linear(layers[-1], out_features=1))
         self.logistic = torch.nn.Sigmoid()
         self.apply(self.init_weights)
